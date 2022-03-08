658e4abbeb5f5dc7b233d243131d785e2206533a
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-08 20:08:01 +0700

    Create __main__.py

---
+++ XMusic/__main__.py
@@ -0,0 +1,65 @@
+import config
+from config import BANNED_USERS
+from XMusic import LOGGER, app, userbot
+from XMusic.core.call import XMusic
+from XMusic.plugins import ALL_MODULES
+from XMusic.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("XMusic").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("XMusic").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("XMusic.plugins" + all_module)
+    LOGGER("XMusic.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await XMusic.start()
+    try:
+        await XMusic.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("XMusic").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await XMusic.decorators()
+    LOGGER("XMusic").info("X Music Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("XMusic").info("Stopping X Music ! GoodBye")
