0a6d7e25839e018e43cf9950f11c5aefa3055ad1
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-09 00:00:52 +0700

    Create ping.py

---
+++ XMusic/plugins/tools/ping.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from XMusic import app
+from XMusic.utils import bot_sys_stats
+from XMusic.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(
+    filters.command(PING_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK)
+    )
