a80785d2328cb02df4fe935ca14cdb6d4dfda2da
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-08 20:02:19 +0700

    Create __init__.py

---
+++ XMusic/__init__.py
@@ -0,0 +1,38 @@
+from XMusic.core.bot import XMusicBot
+from XMusic.core.dir import dirr
+from XMusic.core.git import git
+from XMusic.core.userbot import Userbot
+from XMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = XMusicBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
