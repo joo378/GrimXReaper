d405eaec1728286b4aad188e3dfd36b2cb7189c1
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-10 03:51:42 +0700

    Update thumbnails.py

--- XMusic/utils/thumbnails.py
+++ XMusic/utils/thumbnails.py
@@ -1,4 +1,4 @@
-import os
+     import os
 import re
 import textwrap
 
@@ -6,7 +6,7 @@ import aiofiles
 import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
-from youtubesearchpython import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
@@ -27,7 +27,7 @@ async def gen_thumb(videoid):
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
-        for result in results.result()["result"]:
+        for result in (await results.next())["result"]:
             try:
                 title = result["title"]
                 title = re.sub("\W+", " ", title)
