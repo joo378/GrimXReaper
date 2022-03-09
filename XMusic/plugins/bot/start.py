459e9c979a4e374f9342f38690a61e35fb2b1609
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-10 05:57:50 +0700

    Update start.py

--- XMusic/plugins/bot/start.py
+++ XMusic/plugins/bot/start.py
@@ -167,7 +167,7 @@ async def start_comm(client, message: Message, _):
             OWNER = OWNER_ID[0]
         except:
             OWNER = None
-        out = private_panel(_, app.username, OWNER)
+        out = private_panel(_, app.username)
         if config.START_IMG_URL:
             try:
                 await message.reply_photo(
