ded66be02b29e59dbdfe6ff73c98138781eeb3e1
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-10 04:13:08 +0700

    Update queue.py

--- XMusic/plugins/tools/queue.py
+++ XMusic/plugins/tools/queue.py
@@ -54,9 +54,7 @@ async def ping_com(client, message: Message, _):
                 elif j == 2:
                     msg += f'Queued:\n\n🏷Title: {x["title"]}\nDur: {x["dur"]}\nBy: {x["by"]}\n\n'
                 else:
-                    msg += (
-                        f'🏷Title: {x["title"]}\nDur: {x["dur"]}\nBy: {x["by"]}\n\n'
-                    )
+                    msg += f'🏷Title: {x["title"]}\nDur: {x["dur"]}\nBy: {x["by"]}\n\n'
             if "Queued" in msg:
                 link = await Xbin(msg)
                 lines = msg.count("\n")
