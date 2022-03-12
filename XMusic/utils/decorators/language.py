d117860e3435ec87f76a8f16820fc61edd32709b
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-12 07:05:31 +0700

    Update language.py

--- XMusic/utils/decorators/language.py
+++ XMusic/utils/decorators/language.py
@@ -1,9 +1,16 @@
 from strings import get_string
-from XMusic.utils.database import get_lang, is_commanddelete_on
+from XMusic.misc import SUDOERS
+from XMusic.utils.database import (get_lang, is_commanddelete_on,
+                                   is_maintenance)
 
 
 def language(mystic):
     async def wrapper(_, message, **kwargs):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    "Bot is under maintenance. Please wait for some time..."
+                )
         if await is_commanddelete_on(message.chat.id):
             try:
                 await message.delete()
@@ -13,7 +20,7 @@ def language(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("id")
+            language = get_string("en")
         return await mystic(_, message, language)
 
     return wrapper
@@ -21,11 +28,29 @@ def language(mystic):
 
 def languageCB(mystic):
     async def wrapper(_, CallbackQuery, **kwargs):
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    "Bot is under maintenance. Please wait for some time...",
+                    show_alert=True,
+                )
         try:
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("id")
+            language = get_string("en")
         return await mystic(_, CallbackQuery, language)
+
+    return wrapper
+
+
+def LanguageStart(mystic):
+    async def wrapper(_, message, **kwargs):
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
 
     return wrapper
