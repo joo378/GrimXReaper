586e7448aeb07f095c15c92d4719658008b9fa25
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-12 17:45:28 +0700

    Update maintenance.py

--- XMusic/plugins/sudo/maintenance.py
+++ XMusic/plugins/sudo/maintenance.py
@@ -12,8 +12,12 @@ MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
 
 
 @app.on_message(filters.command(MAINTENANCE_COMMAND) & SUDOERS)
-@language
-async def maintenance(client, message: Message, _):
+async def maintenance(client, message: Message):
+    try:
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+    except:
+        _ = get_string("en")
     usage = _["maint_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
@@ -21,12 +25,20 @@ async def maintenance(client, message: Message, _):
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
     if state == "enable":
-        user_id = 1
-        await add_on(user_id)
-        await message.reply_text(_["maint_2"])
+        if await is_maintenance() is False:
+            await message.reply_text(
+                "Maintenance mode is already enabled"
+            )
+        else:
+            await maintenance_on()
+            await message.reply_text(_["maint_2"])
     elif state == "disable":
-        user_id = 1
-        await add_off(user_id)
-        await message.reply_text(_["maint_3"])
+        if await is_maintenance() is False:
+            await maintenance_off()
+            await message.reply_text(_["maint_3"])
+        else:
+            await message.reply_text(
+                "Maintenance mode is already disabled"
+            )
     else:
         await message.reply_text(usage)
