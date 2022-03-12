36ef0e96b7c676264344d4fc1ce3ac6367f44130
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-12 17:46:17 +0700

    Update sudoers.py

--- XMusic/plugins/sudo/sudoers.py
+++ XMusic/plugins/sudo/sudoers.py
@@ -19,6 +19,10 @@ SUDOUSERS_COMMAND = get_command("SUDOUSERS_COMMAND")
 )
 @language
 async def useradd(client, message: Message, _):
+    if MONGO_DB_URI is None:
+        return await message.reply_text(
+            "**Due to bot's privacy issues, You can't manage sudo users when you're using Yukki's Database.\n\n Please fill your MONGO_DB_URI in your vars to use this feature**"
+        )
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["auth_1"])
@@ -61,6 +65,10 @@ async def useradd(client, message: Message, _):
 )
 @language
 async def userdel(client, message: Message, _):
+    if MONGO_DB_URI is None:
+        return await message.reply_text(
+            "**Due to bot's privacy issues, You can't manage sudo users when you're using Yukki's Database.\n\n Please fill your MONGO_DB_URI in your vars to use this feature**"
+        )
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["auth_1"])
