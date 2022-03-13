2817dbe20efafc7e4dacbbcfd77e39a0ea3a8053
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-13 18:02:14 +0700

    Update playmode.py

--- XMusic/plugins/play/playmode.py
+++ XMusic/plugins/play/playmode.py
@@ -22,7 +22,7 @@ PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
-    if playmode == "Direct":
+    if playmode == "Inline":
         Direct = True
     else:
         Direct = None
