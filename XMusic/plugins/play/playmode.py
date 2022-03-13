5a88eae7016e888d59ed0af7578d0ac395a7e97e
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-13 17:24:17 +0700

    Update playmode.py

--- XMusic/plugins/play/playmode.py
+++ XMusic/plugins/play/playmode.py
@@ -22,10 +22,10 @@ PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
-    if playmode == "Inline":
-        Inline = True
+    if playmode == "Direct":
+        Direct = None
     else:
-        Inline = True
+        Direct = True
     is_non_admin = await is_nonadmin_chat(message.chat.id)
     if not is_non_admin:
         Group = True
@@ -36,7 +36,7 @@ async def playmode_(client, message: Message, _):
         Playtype = None
     else:
         Playtype = True
-    buttons = playmode_users_markup(_, Inline, Group, Playtype)
+    buttons = playmode_users_markup(_, Direct, Group, Playtype)
     response = await message.reply_text(
         _["playmode_1"].format(message.chat.title),
         reply_markup=InlineKeyboardMarkup(buttons),
